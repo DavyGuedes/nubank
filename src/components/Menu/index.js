2a535c10387d080d11942fd24fe8dc48680d95e0
Marcos Davy Guedes Dias <m.davy.br@gmail.com>
2019-07-16 14:07:15 -0300

    Início da implementação do QRCode
    Recursos já implementados:
    - Card com a exibição de saldo e informações sobre última transferência
    - ScrollView com as ações do app Nubank (Indicar, Cobrar, Depositar, Transferir e Bloquear cartão)

---
+++ src/components/Menu/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+import { Container, Code } from './styles';
+
+export default function Menu() {
+  return (
+    <Container>
+      <Code>
+        {/* <QRCode
+          value="https://davyguedes.github.io"
+          size={80}
+          bgColor="#8B10AE"
+          fgColor="#FFF"
+        /> */}
+      </Code>
+    </Container>
+  );
+}
---
+++ src/components/Menu/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.ScrollView.attrs({
+  contentContainerStyle: { alignIitems: 'center' },
+})`
+  margin: 0 30px;
+`;
+
+export const Code = styled.View`
+  background: #FFF;
+`;
